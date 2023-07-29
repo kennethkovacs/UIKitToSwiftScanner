377c7d658c74bf823fd087d8bd67265a9867f5f0
kennethkovacs <17244596+kennethkovacs@users.noreply.github.com>
2023-07-28 23:16:09 -0700

    Initial Commit

---
+++ UIKitToSwiftScanner/UIKitToSwiftScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitToSwiftScannerApp.swift
+//  UIKitToSwiftScanner
+//
+//  Created by Kenneth Kovacs on 2023-07-28.
+//
+
+import SwiftUI
+
+@main
+struct UIKitToSwiftScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
